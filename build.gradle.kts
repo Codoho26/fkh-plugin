8f8d5f1f0ea5b51a3210f86290ce1bdef228789d
faraz.khatami <faraz.khatami@n26.com>
2024-06-06 21:40:55 +0200

    [SXG-5635] Now is fully working. No tests added yet

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("org.jetbrains.intellij") version "1.8.0"
+    id("org.jetbrains.intellij") version "1.10.0"
+    kotlin("jvm") version "1.9.23"
 }
 
 group = "n26.plugin"
@@ -11,37 +11,39 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2021.3.3")
-    type.set("IC") // Target IDE Platform
-
+    version.set("2022.2.3")
     plugins.set(listOf("Git4Idea"))
 }
 
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
 
     patchPluginXml {
         sinceBuild.set("213")
-        untilBuild.set("223.*")
+        untilBuild.set("241.*")
     }
 
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
+//    signPlugin {
+//        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+//        privateKey.set(System.getenv("PRIVATE_KEY"))
+//        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+//    }
 
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
+//    publishPlugin {
+//        token.set(System.getenv("PUBLISH_TOKEN"))
+//    }
 }
--- src/main/kotlin/n26/plugin/n26plugin/CommitMessageAction.kt
+++ src/main/kotlin/n26/plugin/n26plugin/CommitMessageAction.kt
@@ -3,18 +3,23 @@ package n26.plugin.n26plugin
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
-import com.intellij.openapi.vcs.VcsException
-import git4idea.commands.Git
-import git4idea.commands.GitCommand
-import git4idea.commands.GitLineHandler
-import git4idea.repo.GitRepository
+import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.openapi.vcs.changes.CommitExecutor
+import com.intellij.openapi.vcs.changes.ui.CommitChangeListDialog
 import git4idea.repo.GitRepositoryManager
 
 class CommitMessageAction : AnAction() {
 
     override fun actionPerformed(event: AnActionEvent) {
-        val project = event.project ?: return
+        val project = event.project
+
+        if (project == null) {
+            Messages.showErrorDialog("Could not find the project!", "Error")
+            return
+        }
+
 
         val repository = project.service<GitRepositoryManager>().repositories.firstOrNull()
         if (repository == null) {
@@ -27,26 +32,34 @@ class CommitMessageAction : AnAction() {
             Messages.showErrorDialog(project, "No current branch found for project ${project.name}", "Error")
             return
         }
+        Messages.showInfoMessage(project, "parsed branch name: $branchName", "Success")
 
         val jiraTicketNumber = extractJiraTicketNumber(branchName)
 
-        commitChanges(repository, jiraTicketNumber)
+        Messages.showInfoMessage(project, "parsed Jira ticket number: $jiraTicketNumber", "Success")
+
+        openCommitDialog(project, jiraTicketNumber)
     }
 
-    private fun commitChanges(repository: GitRepository, message: String) {
-        val git = Git.getInstance()
-        val handler = GitLineHandler(repository.project, repository.root, GitCommand.COMMIT)
-        handler.addParameters("-m", message)
-        try {
-            git.runCommand(handler).throwOnError()
-            Messages.showInfoMessage(repository.project, "Successfully created commit message with Jira ticket number!", "Success")
-        } catch (e: VcsException) {
-            Messages.showErrorDialog(repository.project, "Failed creating commit message with Jira ticket number!", "Error")
-        }
+    private fun openCommitDialog(project: Project, commitMessage: String) {
+        val changeListManager = ChangeListManager.getInstance(project)
+        val changes = changeListManager.allChanges
+        val initialSelection = changeListManager.defaultChangeList
+        val executors = emptyList<CommitExecutor>()
+
+        CommitChangeListDialog.commitChanges(
+            project,
+            changes,
+            initialSelection,
+            executors,
+            true,
+            commitMessage,
+            null
+        )
     }
 
     private fun extractJiraTicketNumber(branchName: String): String {
         val regex = Regex("^[A-Za-z]+-\\d+")
-        return regex.find(branchName)?.value ?: ""
+        return "[${regex.find(branchName)?.value ?: ""}]"
     }
 }
