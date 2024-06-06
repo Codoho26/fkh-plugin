dbece908d68336ff01b00b3d170ce89db53128d6
faraz.khatami <faraz.khatami@n26.com>
2024-06-06 21:57:48 +0200

    [SXG-5635] Cleaned up

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,15 +15,12 @@ dependencies {
     implementation(kotlin("stdlib"))
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
     version.set("2022.2.3")
     plugins.set(listOf("Git4Idea"))
 }
 
 tasks {
-    // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "17"
         targetCompatibility = "17"
@@ -36,14 +33,4 @@ tasks {
         sinceBuild.set("213")
         untilBuild.set("241.*")
     }
-
-//    signPlugin {
-//        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-//        privateKey.set(System.getenv("PRIVATE_KEY"))
-//        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-//    }
-
-//    publishPlugin {
-//        token.set(System.getenv("PUBLISH_TOKEN"))
-//    }
 }
